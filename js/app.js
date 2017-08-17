44d5898ddca5ffefc89d55915f89218a12529493
Susan Chen <30938981+susanschen@users.noreply.github.com>
2017-08-16 20:44:16 -0400

    create Player object

--- js/app.js
+++ js/app.js
@@ -30,6 +30,12 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(){
+    this.sprite = "images/char-boy.png";
+    this.x = 100;
+    this.y = 0;
+    this.speed = 1;
+};
 
 
 // Now instantiate your objects.
