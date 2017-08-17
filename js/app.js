12961d98b9fda4e128c895d923964346fafdc9a3
Susan Chen <30938981+susanschen@users.noreply.github.com>
2017-08-16 20:47:04 -0400

    create player update prototype

--- js/app.js
+++ js/app.js
@@ -37,6 +37,15 @@ var Player = function(){
     this.speed = 1;
 };
 
+// Update the players's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt){
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    var move = 1;
+    this.x = move * dt;
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
